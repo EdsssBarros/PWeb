8248060c5008845aa66a8d8ea1850370fbed8786
EdsssBarros <edsbarrosjr@gmail.com>
2025-11-12 21:56:10 -0300

    Atividade 14

---
+++ Atividade 14/Pwebnode/Exercicios/Exercicio4promises.js
@@ -0,0 +1,21 @@
+//função para imprimir primeira parte
+function Parte1(){
+    for (let i = 1;i <= 10; i++){
+        console.log(" primeira parte:" + i);
+    }
+}
+
+setTimeout(Parte1, 2000); // para atrasar
+
+const fs = require('fs').promises; //importa a versão com proises
+fs.readFile('file.txt', 'utf8')
+.then(data => {
+    const registros = data.split("\n");
+    registros.forEach((registro, index) => {
+        console.log("segunda parte: " + registro + " " + index);
+    });
+})
+
+.catch(err => {
+    console.error("Erro ao ler o arquivo:", err);
+});
